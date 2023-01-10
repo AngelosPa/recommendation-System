7a419fcbc1908129e3b6d80c539ef27935423424
AngelosPa <mrpalaiologos@gmail.com>
2023-01-10 21:12:11 +0100

    streamlit added

---
+++ index.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("wbsflix")
+# create a searchbar
+search = st.text_input("Search for a movie")
+# create a button
+# enter id
+login = st.text_input("log in")
+button = st.button("log in to wbsflix")
+# GIVE FUNCTIONALITY TO THE BUTTON
+
+st.write("""
+### OUR CUSTOMERS FAVOURITES
+detbgsertaergsdr 
+""")
+# display the context from the popularity function
+
+
+st.write("""
+### BECAUSE U SEARCHED FOR """ + search + """
+detbgsertaergsdr 
+""")
+if button:
+
+    st.write("""
+### FOR U
+detbgsertaergsdr 
+""")
